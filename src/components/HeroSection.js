19fbb02d8e2fdab47cd1b491828a5a69c87278bd
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-07-22 00:24:59 +0300

    Header component designed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,8 +1,48 @@
+import Slider from "react-slick";
+
 
 const HeroSection = () => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  };
+
   return (
-    <div>HeroSection</div>
+    <div className="relative h-[500px] before:absolute before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:top-0 before:inset-0 before:w-full before:h-full before:z-10">
+      <Slider {...settings}>
+        <div>
+          <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg" alt="slider-item" />
+        </div>
+        <div>
+          <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" alt="slider-item" />
+        </div>
+        <div>
+          <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-2-7c23764275cdaf14d7b6cf15ebbdd0c1.jpg" alt="slider-item" />
+        </div>
+        <div>
+          <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-3-ffd1e98c5da51809f9adcc3a4a37d751.jpg" alt="slider-item" />
+        </div>
+      </Slider>
+
+      <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
+        <div>
+          <img src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg" alt="logo" />
+          <h3 className="mt-8 text-4xl font-semibold text-white">
+            Dakikalar içinde <br /> kapınızda
+          </h3>
+        </div>
+        <div className="w-[400px] rounded-lg bg-gray-50 p-6">
+          <h4 className="text-primary-brand-color text-center font-semibold">Giriş yap veya kayıt ol</h4>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
-export default HeroSection
+export default HeroSection;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundColor: theme => ({
-        'brand-color': 'rgb(76, 51, 152)'
+      colors: theme => ({
+        'brand-color': '#4c3398',
+        'primary-brand-color': '#5d3ebc',
+        'secondary-brand-color': '#7849f7'
       })
     },
   },
