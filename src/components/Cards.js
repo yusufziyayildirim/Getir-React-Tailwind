663c5a43c804beab96c623c09220cdd8ff616203
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-07-24 22:55:59 +0300

    Footer component designed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,8 +13,8 @@ const Cards = () => {
   return (
     <div className='container mx-auto mt-10'>
       <div className="grid grid-cols-3 gap-x-4">
-        {cards.length && cards.map(card => (
-          <div className="bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center">
+        {cards.length && cards.map((card,i) => (
+          <div key={i} className="bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center">
             <img className="w-[150px] h-[150px] mb-6" src={card.image} />
             <h6 className="font-semibold text-lg text-primary-brand-color">{card.title}</h6>
             <p className="mt-2 text-sm text-gray-700">{card.description}</p>
