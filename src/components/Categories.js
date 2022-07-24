e6b4866e3eb56b7014c0043b4306650a2fb7fdaa
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-07-24 18:19:52 +0300

    Categories component designed

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import categoryData from 'api/categories';
+import Category from "./ui/Category";
 
 const Categories = () => {
+
+  const[categories, setCategories] = useState([]);
+
+  useEffect(()=>{
+    //İstek
+    setCategories(categoryData);
+  },[])
+
   return (
-    <div>Categories</div>
+    <div className="bg-white p-4">
+      <div className="container mx-auto">
+        <h3 className="text-sm font-semibold mb-3">Kategoriler</h3>
+        <div className="grid grid-cols-10">
+          {}
+          {categories && categories.map((category, i)=> 
+            <Category key={i} category={category} />
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/ui/Category.js
@@ -0,0 +1,12 @@
+
+const Category = ({category}) => {
+  return (
+    <a href="" className="flex flex-col items-center gap-y-2 p-4 group hover:bg-[#f3f0fe] transition-colors">
+        <img src={category.image} alt={category.title} className="w-12 h-12 border border-gray-200 rounded " />
+        <span className="text-sm font-semibold text-gray-700 group-hover:text-brand-color tracking-tight">{category.title}</span>
+        
+    </a>
+  )
+}
+
+export default Category;
